40ece66f07d3fea7ccc92c90f29a0c3efbf59115
root <root@Germes.localdomain>
2021-07-07 12:42:42 +0300

    end project(it's not mine project, i create it with courses

--- src/routes.py
+++ src/routes.py
@@ -1,48 +1,13 @@
 from src import api
-from flask_restful import Resource
-
-
-class FilmListApi(Resource):
-    def get(self, uuid=None):
-        if not uuid:
-            films = db.session.query(Film).all()
-            return [f.to_dict() for f in films], 200
-        film = db.session.query(Film).filter_by(uuid=uuid).first()
-        if not film:
-            return ',404'
-        return film.to_dict(),200
-
-    def post(self):
-        film_json = request.json
-        if not film_json:
-            return {'message': 'Wrong data'}, 400
-        try:
-            film = Film(
-                title=film_json['title'],
-                release_date=datetime.strptime(film_json['release_date'], '%B %d, %Y'),
-
-            )
-
-    def put(self):
-        pass
-
-    def delete(self):
-        pass
-
-
-class ActorListApi(Resource):
-    def get(self, uuid=None):
-        pass
-
-    def post(self):
-        pass
-
-    def put(self):
-        pass
-
-    def delete(self):
-        pass
-
+from src.resources.actors import ActorListApi
+from src.resources.aggregations import AggregationApi
+from src.resources.auth import AuthRegister, AuthLogin
+from src.resources.films import FilmListApi
+from src.resources.smoke import Smoke
 
 api.add_resource(Smoke, '/smoke', strict_slashes=False)
-api.add_resource(FilmListApi, '/films/<uuid>', strict_slashes=False)
+api.add_resource(FilmListApi, '/films', '/films/<uuid>', strict_slashes=False)
+api.add_resource(ActorListApi, '/actors', '/actors/<uuid>', strict_slashes=False)
+api.add_resource(AggregationApi, '/aggregations', strict_slashes=False)
+api.add_resource(AuthRegister, '/register', strict_slashes=False)
+api.add_resource(AuthLogin, '/login', strict_slashes=False)
